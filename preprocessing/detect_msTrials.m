b0b0a7b635db31e4688d2a2c586dd79121fe72f0
kendranoneman <knoneman@andrew.cmu.edu>
2023-03-23 16:34:34 -0400

    adding microsacc detection function details

--- preprocessing/detect_msTrials.m
+++ preprocessing/detect_msTrials.m
@@ -1,4 +1,23 @@
 function msFlag = detect_msTrials(eye,stimOnset,preint,postint,accThresh,velThresh)
+% OBJECTIVE:
+% determine if nhp made microsaccade around stimulus onset in given trial
+%
+% INPUTS:
+% eye = 4x1 cell array w/ (HEPos, VEPos, HEVel, VEVel)
+% stimOnset = time of target motion onset or other stimulus onset
+% preint = time before stimOnset you want to include 
+% postint = time after stimOnset you want to include
+% accThresh = acceleration threshold for microsacc detection (e.g. 750 deg/s^2)
+% velThresh = velocity threshold for microsacc detection (e.g. 50 deg/s)
+%
+% OUTPUTS:
+% msFlag = 1 if microsacc detected, 0 if not detected
+
+if nargin < 3
+    preint = 50; postint = 50; accThresh = 750; velThresh = 50;
+elseif nargin < 5
+    accThresh = 750; velThresh = 50;
+end
 
 % smooth velocities
 hVel = smoothdata(eye{3},'gaussian',20); 
@@ -8,7 +27,7 @@ vVel = smoothdata(eye{4},'gaussian',20);
 x = (1:length(rVel));
 rAcc = (gradient(rVel(:)) ./ gradient(x(:)./1000));
 
-% Detect saccades occurring in the window [-51 100], too early in the trial
+% Detect saccades occurring in the window [-preint postint], where 0 = stim onset
 if sum(abs(rAcc(stimOnset-preint:stimOnset+postint))>accThresh | abs(rVel(stimOnset-preint:stimOnset+postint))'>velThresh)
     msFlag = 1;
 else
