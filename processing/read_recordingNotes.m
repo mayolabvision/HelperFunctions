3f0b21079b9df5960f94a7a9a2e673b35f86d751
kendranoneman <knoneman@andrew.cmu.edu>
2025-03-11 15:58:43 -0400

    more changes to processing script, prep for trying on h2p

--- processing/read_recordingNotes.m
+++ processing/read_recordingNotes.m
@@ -10,12 +10,9 @@ function [this_sess,filename] = read_recordingNotes(NOTES_PATH,EXPERIMENTER,MONK
     recordings.session_depth = categorical(recordings.session_depth);
     recordings.mapFile_name = categorical(recordings.mapFile_name );
 
-    recordings.hemi = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.hemi, 'uni', 0);
-    recordings.gridHole = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.gridHole, 'uni', 0);
     recordings.gtHeight_mm = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.gtHeight_mm, 'uni', 0);
     recordings.recordDepth_mm = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.recordDepth_mm, 'uni', 0);
     recordings.probeID = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.probeID, 'uni', 0);
-    recordings.probeUse_num = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.probeUse_num, 'uni', 0);
     recordings.dead_ripChans = cellfun(@(q) eval(strrep(strrep(q, '“', '"'), '”', '"')), recordings.dead_ripChans, 'uni', 0);
     
     this_sess = recordings(recordings.experimenter==EXPERIMENTER & recordings.monkey==MONKEY & recordings.session_depth==SESSION,:);
