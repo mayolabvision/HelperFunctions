883a1ab9c953367ee04b65e01352a8114b832a4a
kendranoneman <knoneman@andrew.cmu.edu>
2025-06-18 16:03:54 -0400

    updates to rfmp plot local

--- analyses/ia_rfMaps.m
+++ analyses/ia_rfMaps.m
@@ -58,13 +58,15 @@ function ia_rfMaps(data_path,varargin)
     fields = fieldnames(S);
     matchingFields = fields(contains(fields, {'rfmp', 'rfMapping'}, 'IgnoreCase', true));
 
-    num_conditions = [];
+    task_conditions = zeros(length(matchingFields),2);
     for mm = 1:numel(matchingFields)
         stimuli = cell2mat(vertcat(S.(matchingFields{mm}).tbl.conditions{:}));
-        num_conditions = [num_conditions; length(sort(unique(stimuli(:,1))).') * length(sort(unique(stimuli(:,2))).')];
+        task_conditions(mm,:) = [length(sort(unique(stimuli(:,1))).') * length(sort(unique(stimuli(:,2))).') S.(matchingFields{mm}).params.frameCount];
     end
 
-    if numel(unique(num_conditions))==1
+    [unique_conditions, ~, row_conds] = unique(task_conditions, 'rows', 'stable');
+
+    if numel(unique(row_conds))==1
         if ~exist(FIG_PATH, 'dir'), mkdir(FIG_PATH); end    
         T = []; 
         for mm = 1:numel(matchingFields)
@@ -101,34 +103,47 @@ function ia_rfMaps(data_path,varargin)
             end
         end
     else
-        for mm = 1:numel(matchingFields)
-            T = S.(matchingFields{mm}).tbl;
-            if ~exist(fullfile(FIG_PATH, matchingFields{mm}), 'dir'), mkdir(fullfile(FIG_PATH, matchingFields{mm})); end    
+        for mm = 1:numel(size(unique_conditions,1))
+            these_rows = find(row_conds==mm);
+            T = []; 
+            for r = 1:numel(these_rows)
+                T = [T; S.(matchingFields{r}).tbl];
+            end
+
+            stim_duration_ms = T.params(1,1).block.frameCount*(1000/120);
+            
+            if ~exist(fullfile(FIG_PATH, strjoin(matchingFields(these_rows), '_')), 'dir'), mkdir(fullfile(FIG_PATH, strjoin(matchingFields(these_rows), '_'))); end    
 
             for u=1:length(units)
                 unit = units(u); 
-                if ~exist(fullfile(FIG_PATH, matchingFields{mm}, sprintf('imec%d_unit%04d_chan%03d.png', IMEC, unit, chans(u))), 'file')
+                if ~exist(fullfile(FIG_PATH, strjoin(matchingFields(these_rows), '_'), sprintf('imec%d_unit%04d_chan%03d.png', IMEC, unit, chans(u))), 'file')
                     [frs,bin_edges,xvals,yvals] = format_tableToRFMap(T, 'IMEC', IMEC, 'UNITS', (unit+1));
                 
-                    f2a = figure('Visible','off');
+                    f2a = figure; %('Visible','off');
                     f2a.Position = [100 100 1800 900];
                     tl = heatMap_rfOverTime(frs{1},'BIN_EDGES',bin_edges, 'INTERP', false,'X_VALS',xvals, 'Y_VALS',yvals);
                     
                     if (IMEC)==0
-                        title(tl,sprintf('%s_%s --- LEFT --- cluster %d (channel %d)',filename, matchingFields{mm}, unit, chans(u)),'fontsize',16,'interpreter','none')
+                        title(tl,sprintf('%s_%s --- LEFT --- cluster %d (channel %d)',filename, strjoin(matchingFields(these_rows), '_'), unit, chans(u)),'fontsize',16,'interpreter','none')
                     else
-                        title(tl,sprintf('%s_%s --- RIGHT --- cluster %d (channel %d)',filename, matchingFields{mm}, unit, chans(u)),'fontsize',16,'interpreter','none')
+                        title(tl,sprintf('%s_%s --- RIGHT --- cluster %d (channel %d)',filename, strjoin(matchingFields(these_rows), '_'), unit, chans(u)),'fontsize',16,'interpreter','none')
                     end
                     subtitle(tl, sprintf('ks_label = %s, snr = %.4f, y_pos = %.2f um', kslabs(u), snrs(u), depths(u)),'fontsize',12,'interpreter','none')
                     
-                    annotation('textbox', [0.8 0.89 0.2 0.1], ... % [x y w h] in normalized figure units
+                    annotation('textbox', [0.77 0.89 0.2 0.1], ... % [x y w h] in normalized figure units
                         'String', sprintf('N = %d repeats', min(min(min(cellfun(@length, frs{1}))))), ...
                         'FontSize', 16, ...
                         'EdgeColor', 'none', ...
                         'HorizontalAlignment', 'right');
+
+                    annotation('textbox', [0.01 0.89 0.2 0.1], ... % [x y w h] in normalized figure units
+                        'String', sprintf('probe duration = %.1f ms', stim_duration_ms), ...
+                        'FontSize', 16, ...
+                        'EdgeColor', 'none', ...
+                        'HorizontalAlignment', 'left');
                     
 
-                    print(f2a, fullfile(FIG_PATH, matchingFields{mm}, sprintf('imec%d_unit%04d_chan%03d.png', IMEC, unit, chans(u))), '-dpng', '-r200');
+                    print(f2a, fullfile(FIG_PATH, strjoin(matchingFields(these_rows), '_'), sprintf('imec%d_unit%04d_chan%03d.png', IMEC, unit, chans(u))), '-dpng', '-r200');
                     fprintf(sprintf('\n----IMEC %d, Unit %.4d COMPLETE----',IMEC, unit))
                 else
                     fprintf(sprintf('\n----IMEC %d, Unit %.4d exists----',IMEC, unit))
