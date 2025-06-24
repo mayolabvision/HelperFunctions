b7d898d2d362e6f4453a6456a59d48200fa1923a
knoneman <knoneman@andrew.cmu.edu>
2025-06-24 14:23:42 -0400

    more memory for table submit script

--- data_io/submit_makeTable_behavOnly.sh
+++ data_io/submit_makeTable_behavOnly.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=16G
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=16
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=knoneman@pitt.edu
 #SBATCH --time=0-02:00:00
