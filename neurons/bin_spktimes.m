6efa2621100f367aa6e59a2bb91b14b04243e9ec
kendranoneman <knoneman@andrew.cmu.edu>
2025-10-24 16:38:14 -0400

    updating fxn to be like in nhppipe

--- neurons/bin_spktimes.m
+++ neurons/bin_spktimes.m
@@ -1,11 +1,29 @@
-function spks_binned = bin_spktimes(spktimes,startTime,endTime,binSize)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function spks_binned = bin_spktimes(spktimes, varargin)
+% bin_spktimes - Bin spike times into fixed-size bins.
+%
+% Usage:
+%   spks_binned = bin_spktimes(spktimes)
+%   spks_binned = bin_spktimes(spktimes, 'StartTime', 0, 'EndTime', 1000, 'BinSize', 50)
+%
+% Defaults:
+%   StartTime = 0
+%   EndTime   = max(spktimes)
+%   BinSize   = 50
 
-edges = startTime : binSize : endTime;
+    % set up parser
+    p = inputParser;
+    addRequired(p, 'spktimes', @(x) isnumeric(x));
+    addParameter(p, 'StartTime', 0, @(x) isnumeric(x) && isscalar(x));
+    addParameter(p, 'EndTime', max(spktimes), @(x) isnumeric(x) && isscalar(x));
+    addParameter(p, 'BinSize', 50, @(x) isnumeric(x) && isscalar(x));
 
-allCounts = zeros(1, length(edges) - 1);
-[counts, values] = histcounts(spktimes, edges);
-spks_binned = allCounts + counts;
+    parse(p, spktimes, varargin{:});
+    startTime = p.Results.StartTime;
+    endTime   = p.Results.EndTime;
+    binSize   = p.Results.BinSize;
 
+    % binning
+    edges = startTime:binSize:endTime;
+    [counts, ~] = histcounts(spktimes, edges);
+    spks_binned = counts;
 end
